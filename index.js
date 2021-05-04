5a405f9d2d4db58f40d3ce376e5a5317df9c6e51
jethro revalde <jethro.revalde@hotmail.com>
2021-05-05 09:52:44 +1000

    Final version of this site. perhaps needs changes to search bar and pagination and dropdowns into forms?

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//nothing in here yet
-
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -19,3 +17,23 @@ function myFunction() {
       }
     }
   }
+
+
+  function myFuunction() {
+    document.getElementById("myDroopdown").classList.toggle("shoow");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.droopbtn')) {
+      var dropdownss = document.getElementsByClassName("droopdown-content");
+      var i;
+      for (i = 0; i < dropdownss.length; i++) {
+        var openDroopdown = dropdownss[i];
+        if (openDroopdown.classList.contains('shoow')) {
+          openDroopdown.classList.remove('shoow');
+        }
+      }
+    }
+  } 
+  
